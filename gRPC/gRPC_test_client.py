3d3c3985971d6418b79f0fd7353019ef446e8839
Marcel Canhisares <marcel@canhisares.com>
2018-07-06 17:38:06 -0300

    Mudando assinatura e nome de metodos

--- gRPC/gRPC_test_client.py
+++ gRPC/gRPC_test_client.py
@@ -16,11 +16,35 @@
 from __future__ import print_function
 
 import grpc
+import time
 
 import helloworld_pb2
 import helloworld_pb2_grpc
 
+#time measuring
+file = open('result.html', 'w')
+function_tries = 50
 
+#html headers
+file.write("<html><head><title>Timing</title></head><body><table><tr><th>function</th><th>time(ms)</th></tr>")
+
+def timing(f):
+    def wrap(*args):
+        total = 0
+        ret = None
+        for i in range(function_tries):
+            time1 = time.time()
+            ret = f(*args)
+            time2 = time.time()
+            total = total + (time2 - time1)
+        file.write("<tr>")
+        file.write("<td>"+f.__name__+"</td>")
+        file.write("<td>"+str(total*100)+"</td>")
+        file.write("</tr>")
+        return ret
+    return wrap
+
+@timing
 def run():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
@@ -35,4 +59,5 @@ def run():
 
 if __name__ == '__main__':
     print ("Iniciando o client...")
+
     run()
